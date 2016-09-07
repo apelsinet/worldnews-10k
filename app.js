227bc4220a921f5844ed333800e92b8a13dc60c4
apelsinet <mattiasprada@gmail.com>
2016-09-07 22:43:39 +0200

    Remove images in third col on mobile. Clean up css. Add level 9 gzip.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var compression = require('compression');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -16,6 +17,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(compression({level: 9, threshold: false}));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
