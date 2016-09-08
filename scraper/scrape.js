9ac21979799a6d68ad8e018d2611fdb96ebeaa24
apelsinet <mattiasprada@gmail.com>
2016-09-09 00:56:14 +0200

    Add reddit api json fetcher and parser. Connect parsed json with scraper. Scraper not working on Reuters.com for some reason, checking back tomorrow.

--- scraper/scrape.js
+++ scraper/scrape.js
@@ -1,14 +1,45 @@
 const metascrape = require('metascrape');
 const wget = require('node-wget');
-var IMG = '';
+const jsonfile = require('jsonfile');
+const path = require('path');
 
-metascrape.fetch('http://www.sciencealert.com/costa-rica-has-been-running-on-100-renewable-energy-for-2-months-straight', 1000).then((response) => {
-for (type in response) {
-  if (type == 'openGraph') {
-    console.log(response[type].description);
-    IMG = response[type].image;
-    wget(IMG);
+/*
+   wget({
+   url: 'https://www.reddit.com/r/worldnews/.json?limit=10',
+   dest: './scraper/worldnews.json'
+   });
+   */
+
+const file = './scraper/worldnews.json';
+jsonfile.readFile(file, function(err, obj) {
+  for(let i = 0; i < 10; i++) {
+    console.log('----------');
+    console.log('Article: ' + i + '.');
+    console.log('Article URL: ' + obj.data.children[i].data.url);
+    console.log('Article title: ' + obj.data.children[i].data.title);
+    console.log('Comments URL: https://www.reddit.com' + obj.data.children[i].data.permalink);
+    console.log('Comments count: ' + obj.data.children[i].data.num_comments);
+
+    metascrape.fetch(obj.data.children[i].data.url, 1000).then((response) => {
+      for (type in response) {
+        if (type == 'openGraph') {
+          console.log('Article: ' + i + '.');
+          console.log('Description: \n' + response[type].description + '\n');
+          let img = response[type].image;
+          let fileType = path.extname(img);
+
+          wget({
+            url: img,
+            dest: './scraper/img/' + i + fileType
+          });
+
+          console.log('Saved image to ./scraper/img/' + i + fileType + '\n');
+          console.log('----------');
+          break;
+        }
+      }
+    });
   }
-}
 });
+
 
