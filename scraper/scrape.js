43a175534763486f92e60460a8e4d4d600ca405b
apelsinet <mattiasprada@gmail.com>
2016-09-09 17:11:46 +0200

    Make json fetch promise based.

--- scraper/scrape.js
+++ scraper/scrape.js
@@ -1,30 +1,30 @@
 const metascrape = require('metascrape');
 const wget = require('node-wget');
-const jsonfile = require('jsonfile');
 const path = require('path');
+const fetch = require('node-fetch');
 
-/*
-   wget({
-   url: 'https://www.reddit.com/r/worldnews/.json?limit=10',
-   dest: './scraper/worldnews.json'
-   });
- */ 
+/*wget({
+  url: 'https://www.reddit.com/r/worldnews/.json?limit=10',
+  dest: './scraper/worldnews.json'
+  });*/
 
-const file = './scraper/worldnews.json';
-jsonfile.readFile(file, function(err, obj) {
+fetch('https://www.reddit.com/r/worldnews/.json?limit=10')
+.then(function(res) {
+  return res.json();
+}).then(function(json) {
+  //console.log(json);
+
   for(let i = 0; i < 10; i++) {
-    console.log('----------');
-    console.log('Article: ' + i + '.');
-    console.log('Article URL: ' + obj.data.children[i].data.url);
-    console.log('Article title: ' + obj.data.children[i].data.title);
-    console.log('Comments URL: https://www.reddit.com' + obj.data.children[i].data.permalink);
-    console.log('Comments count: ' + obj.data.children[i].data.num_comments);
-
-    metascrape.fetch(obj.data.children[i].data.url, 1000).then((response) => {
+        metascrape.fetch(json.data.children[i].data.url, 1000).then((response) => {
       for (type in response) {
         if (type == 'openGraph') {
+          console.log('\n----------');
           console.log('Article: ' + i + '.');
-          console.log('Description: \n' + response[type].description + '\n');
+          console.log('Article URL: ' + json.data.children[i].data.url);
+          console.log('Article title: ' + json.data.children[i].data.title);
+          console.log('Article description: ' + response[type].description);
+          console.log('Comments URL: https://www.reddit.com' + json.data.children[i].data.permalink);
+          console.log('Comments count: ' + json.data.children[i].data.num_comments);
 
           let img = response[type].image;
           let fileType = path.extname(img);
@@ -37,12 +37,15 @@ jsonfile.readFile(file, function(err, obj) {
           });
 
           console.log('Saved image to ./scraper/img/' + i + fileType + '\n');
-          console.log('----------');
           break;
         }
       }
     });
   }
+
+
+
+
 });
 
 
