3c004551bfeecf9a0da1e7a3661fe03dadd76188
apelsinet <mattiasprada@gmail.com>
2016-09-18 20:58:10 +0200

    Add promise for whole scraper.

--- scraper/main.js
+++
@@ -1,8 +0,0 @@
-const testModule = require('./modules/testModule');
-
-
-testModule.testFunction(2).then(result => {
-  console.log(result);
-}).catch(e => {
-  console.error(e);
-});
--- scraper/modules/testModule.js
+++
@@ -1,9 +0,0 @@
-const TestModule = {
-  testFunction: (x) => new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(x * 2);
-    }, 1000);
-  })
-}
-
-module.exports = TestModule;
