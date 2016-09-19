ab99d2cbb57f17e0901e62d4bcae4b4efdfdc65c
apelsinet <mattiasprada@gmail.com>
2016-09-19 22:21:24 +0200

    Insert titles and descriptions in template engine.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+const scraper = require('../scraper/scrape');
 
+let obj;
+scraper.run().then(result => {
+  obj = result;
+})
+.catch(err => {
+  console.log(err);
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'WorldNews 10K' });
+  res.render('index', {
+    title: 'WorldNews 10K',
+    a0title: obj[0].title,
+    a1title: obj[1].title,
+    a2title: obj[2].title,
+    a3title: obj[3].title,
+    a4title: obj[4].title,
+    a5title: obj[5].title,
+    a6title: obj[6].title,
+    a7title: obj[7].title,
+    a8title: obj[8].title,
+    a9title: obj[9].title,
+    a0desc: obj[0].desc,
+    a1desc: obj[1].desc,
+    a2desc: obj[2].desc,
+    a3desc: obj[3].desc,
+    a4desc: obj[4].desc,
+    a5desc: obj[5].desc,
+    a6desc: obj[6].desc,
+    a7desc: obj[7].desc,
+    a8desc: obj[8].desc,
+    a9desc: obj[9].desc
+  });
 });
 
 module.exports = router;
--- scraper/scrape.js
+++ scraper/scrape.js
@@ -48,34 +48,31 @@ const Scraper = {
 
                   obj = storeArticleData(obj, json, metaData, i);
                   const img = sanitizeImageURL(metaData.image, i);
-                  fetch(img)
-                    .then(res => {
+                  fetch(img).then(res => {
 
-                      if (res.status != 200) {
-                        console.log('Status: ' + res.status + '. Using generic image.');
-                        fs.writeFileSync(constants.IMG_DIR + i, fs.readFileSync('./dist/not_found.png'));
-                        return readChunk.sync('./dist/not_found.png', 0, 12);
-                      }
-                      else {
-                        res.body.on('error', err => {
-                          console.log('Error fetching image: ' + i + '.\n' + err);
-                          articleReject(i);
-                        });
-                        res.body.pipe(fs.createWriteStream(constants.IMG_DIR + i));
-                        return res.buffer();
-                      }
+                    if (res.status != 200) {
+                      console.log('Status: ' + res.status + '. Using generic image.');
+                      fs.writeFileSync(constants.IMG_DIR + i, fs.readFileSync('./dist/not_found.png'));
+                      return readChunk.sync('./dist/not_found.png', 0, 12);
+                    }
+                    else {
+                      res.body.on('error', err => {
+                        console.log('Error fetching image: ' + i + '.\n' + err);
+                        articleReject(i);
+                      });
+                      res.body.pipe(fs.createWriteStream(constants.IMG_DIR + i));
+                      return res.buffer();
+                    }
 
-                    }).then(buffer => {
-                      obj[i].imgFormat = imageType(buffer).ext;
-                      obj = convertImages(obj, i);
-                      articleResolve(i);
-                    }).catch(err => {
-                      console.log('Could not fetch or store image: ' + img);
-                      console.log(err);
-                      articleReject(i);
-                    });
-
-
+                  }).then(buffer => {
+                    obj[i].imgFormat = imageType(buffer).ext;
+                    obj = convertImages(obj, i);
+                    articleResolve(i);
+                  }).catch(err => {
+                    console.log('Could not fetch or store image: ' + img);
+                    console.log(err);
+                    articleReject(i);
+                  });
 
                 }).catch(err => {
                   console.log('Could not scrape data from: ' + json.data.children[i].data.url);
