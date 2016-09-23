06c0531be055a74dd7c7f3b061c90ba7a76e06d4
apelsinet <mattiasprada@gmail.com>
2016-09-24 00:23:28 +0200

    Add quote fixer function for titles and descriptions.

--- scraper/modules/storeArticleData.js
+++ scraper/modules/storeArticleData.js
@@ -6,11 +6,33 @@ const storeArticleData = (obj, json, metaData, i) => {
     s = s.substring(0, n != -1 ? n : s.length);
     return s;
   }
+
   let shortenString = (string, threshold) => {
     let s = string;
     var n = s.indexOf(' ', threshold);
     s = s.substring(0, n != -1 ? n : s.length);
     s = s.concat(' ...');
+    return s;
+  }
+
+  let fixQuotes = (string) => {
+    let s = string;
+    let stringReplacer = (quoteType) => {
+      let count = 0;
+      while(s.indexOf(quoteType) !== -1) {
+        if (count % 2 === 0) {
+          s = s.replace(quoteType, '\u201C');
+        }
+        else {
+          s = s.replace(quoteType, '\u201D');
+        }
+        count++;
+      }
+    }
+    if (s.indexOf('&#34;') !== -1) stringReplacer('&#34;');
+    if (s.indexOf('\u0022') !== -1) stringReplacer('\u0022');
+    if (s.indexOf('&quot;') !== -1) stringReplacer('&quot;');
+    if (s.indexOf('&#x22;') !== -1) stringReplacer('&#x22;');
     return s;
   }
 
@@ -24,6 +46,7 @@ const storeArticleData = (obj, json, metaData, i) => {
     obj[i].title = shortenString(obj[i].title, 150);
   }
   obj[i].title = obj[i].title.trim();
+  obj[i].title = fixQuotes(obj[i].title);
 
   if (metaData.description != undefined) {
     // Trim whitespace and newlines from string.
@@ -31,6 +54,7 @@ const storeArticleData = (obj, json, metaData, i) => {
     if (obj[i].desc.length > 300) {
       obj[i].desc = shortenString(obj[i].desc, 300);
     }
+    obj[i].desc = fixQuotes(metaData.description);
   }
   else {
     console.log('Article description for article #' + i + ' not found. Using empty string.');
