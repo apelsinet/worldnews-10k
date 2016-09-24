ca3dfd5fe9d3251414ad5576ac1d00975d75b245
apelsinet <mattiasprada@gmail.com>
2016-09-24 16:25:35 +0200

    Clean up image url sanitize function since it was not letting through all images.

--- scraper/modules/sanitizeImageURL.js
+++ scraper/modules/sanitizeImageURL.js
@@ -1,23 +1,7 @@
-const path = require('path');
-
 const sanitizeImageURL = (imgURL, i) => {
-  let img = '';
-  if (imgURL != undefined) {
-    img = imgURL;
-  }
-  else {
+  let img = imgURL;
+  if (img == undefined || img == '') {
     console.log('Image for article ' + i + ' not found.');
-    return false;
-  }
-  let fileType = path.extname(img);
-  if (path.extname(img) == '') {
-    fileType = '.jpg';
-  }
-  let questionMark = fileType.indexOf('?');
-  fileType = fileType.substring(0, questionMark != -1 ? questionMark : fileType.length);
-
-  if (fileType != '.jpg' && fileType != '.jpeg' && fileType != '.png' && fileType != '.gif') {
-    console.log('Unknown file type for article ' + i + '.');
     return false;
   }
   return img;
