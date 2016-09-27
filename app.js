48173acf248f7c7f80174826cc5f8529fb48501f
apelsinet <mattiasprada@gmail.com>
2016-09-27 02:31:40 +0200

    Remove node-sass temporarily because it's crashing azure instance.

--- app.js
+++ app.js
@@ -26,14 +26,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('node-sass-middleware')({
+/*app.use(require('node-sass-middleware')({
   src: path.join(__dirname, 'dist'),
   dest: path.join(__dirname, 'dist'),
   indentedSyntax: true,
   sourceMap: false,
   outputStyle: 'compressed',
   maxAge: CACHE_TIME
-}));
+}));*/
 app.use(express.static(path.join(__dirname, 'dist'), {maxAge: CACHE_TIME}));
 
 app.use('/', routes);
