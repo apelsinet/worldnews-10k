ff915877ff56f9a07f0e0e6b83c3d52a0eb355ab
apelsinet <mattiasprada@gmail.com>
2016-09-27 15:02:20 +0000

    Add 30 sec sleep between daemons in start script.

--- start.sh
+++ start.sh
@@ -7,4 +7,6 @@
 
 NODE_ENV=production
 forever start -a -e scraperErr.log -o scraperOut.log job.js
+echo "Sleeping 30 seconds while scraper is running."
+sleep 30s
 forever start -a -e serverErr.log -o serverOut.log ./bin/www
