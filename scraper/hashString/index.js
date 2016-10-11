56c46f0a9d24a82d204adafbd19c7979973b540c
apelsinet <mattiasprada@gmail.com>
2016-10-11 11:12:19 +0200

    Shorten image hashes to 10 chars.

--- scraper/hashString/index.js
+++ scraper/hashString/index.js
@@ -1,5 +1,5 @@
 const crypto = require('crypto');
 
 module.exports = (string) => {
-  return crypto.createHash('md5').update(string).digest('hex');
+  return crypto.createHash('md5').update(string).digest('hex').slice(0, 10);
 }
