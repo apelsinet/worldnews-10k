78ff88445db70e082dfcd50376be421fd9314150
apelsinet <mattiasprada@gmail.com>
2016-10-17 01:06:29 +0200

    Add event that fires archiver.

---
+++ archive/README.md
@@ -0,0 +1,1 @@
+This is where the archived files are stored.
--- job.js
+++ job.js
@@ -1,10 +1,13 @@
 'use strict';
+const fs = require('fs');
+const archiver = require('archiver');
 const scraper = require('./scraper');
 const fileExists = require('./scraper/fileExists');
-const fs = require('fs');
 const DATA_JSON = './data.json';
+const CACHE = './scraperCache.json';
 const minutes = 5, scraperInterval = minutes * 60 * 1000;
 const dev = process.env.NODE_ENV === 'development' ? true : false;
+let archiverHasRun;
 
 const runScraper = () => {
   let retriesRemaining = 5;
@@ -21,12 +24,27 @@ const runScraper = () => {
         }
       }
       console.log('JSON file written.');
+      const now = new Date();
+      if (/*now.getHours() === 3 && now.getMinutes() <= minutes && */archiverHasRun !== now.getDate()) {
+        runArchiver(result).then(() => {
+          archiverHasRun = now.getDate();
+          runScraper();
+        });
+      }
       if (dev) console.log(result);
     });
   }).catch(err => {
     console.log(err);
   });
 }
+
+const runArchiver = (data) => new Promise((resolve, reject) => {
+  const now = new Date();
+  const dateString = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate();
+  console.log(dateString);
+  resolve();
+
+});
 
 // Run scraper on server start.
 runScraper();
